8e43a012225c5ad8f794cfb7c27af37bc519f17e
勝貴 佐藤 <190797@jc-21.jp>
2021-11-26 11:34:20 +0900

    変更８

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -15,6 +15,46 @@ namespace R03BMI
             InitializeComponent();
             creater.Text = "JK3A17 佐藤　勝貴";
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double h = Convert.ToInt32(heighit);
+            double w = Convert.ToInt32(weighit);
+            if (h <= 3)
+            {
+                double heighit = h / 100;
+            }
+            else if (w <= 1000) ;
+            {
+                double weighit = w / 1000;
+            }
+            //計算
+            Double bmi = ((h * h) / w);
+            //表示
+            if (bmi < 18.5)
+            {
+                result.Text = (bmi + "低体重（瘦せ）");
+            }
+            else if (18.5 < bmi && bmi < 25.0)
+            {
+                result.Text = (bmi + "普通体重");
+            }
+            else if (25.0 < bmi && bmi < 30.0)
+            {
+                result.Text = (bmi + "肥満（1度）");
+            }
+            else if (30.0 < bmi && bmi < 35.0)
+            {
+                result.Text = (bmi + "肥満（2度）");
+            }
+            else if (35.0 < bmi && bmi < 40.0)
+            {
+                result.Text = (bmi + "肥満（3度）");
+            }
+            else if (40.0 < bmi && bmi < 45.0)
+            {
+                result.Text = (bmi + "肥満（4度）");
+            }
+        }
     }
 }
-       
